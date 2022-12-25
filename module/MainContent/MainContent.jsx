d5baed246fb7de23632d8351111e1fd600e16320
Olga <75717471+Folalika@users.noreply.github.com>
2022-12-25 23:09:37 +0300

    LABA3
    3

---
+++ module/MainContent/MainContent.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+
+const MainStyled = styled.div`
+  color: white;
+  font-family: sans-serif;
+  font-size: 20px;
+  text-align: left;
+    margin-left: 150px;
+    margin-right: 150px;
+    margin-top: 0;
+    margin-bottom: 0;
+    font-weight: 200;
+    line-height: 1.5;
+    
+`
+
+export function MainContent() {
+    return <div><MainStyled><div></div><div></div>
+    Ко́шка — домашнее животное, одно из наиболее популярных «животных-компаньонов». 
+    С точки зрения научной систематики, 
+    домашняя кошка — млекопитающее семейства кошачьих отряда хищных. 
+    Одни исследователи рассматривают домашнюю кошку как подвид дикой кошки, 
+    другие — как отдельный биологический вид. 
+</MainStyled></div >
+}
